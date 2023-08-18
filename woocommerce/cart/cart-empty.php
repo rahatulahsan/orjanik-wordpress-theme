3dccef779050b4be159c1b9480e63e4b351ccf76
rahatulahsan <rahatulahsan@gmail.com>
2023-08-18 23:17:15 +0600

    Checkout and Order Receipt Page completed

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -23,17 +23,20 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_cart_is_empty' );
 
 if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-	<p class="return-to-shop">
-		<a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php
-				/**
-				 * Filter "Return To Shop" text.
-				 *
-				 * @since 4.6.0
-				 * @param string $default_text Default text.
-				 */
-				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
-			?>
-		</a>
-	</p>
+	<div class="col-md-12">
+		<p class="return-to-shop empty_cart">
+			<a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+				<?php
+					/**
+					 * Filter "Return To Shop" text.
+					 *
+					 * @since 4.6.0
+					 * @param string $default_text Default text.
+					 */
+					echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
+				?>
+			</a>
+		</p>
+	</div>
+	
 <?php endif; ?>
